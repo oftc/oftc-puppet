a1ccce083acf0e9a8fd46af94029c84696451309
Christoph Berg <myon@debian.org>
2015-03-20 13:49:15 +0100

    Set pluginsync true for 2.x clients

--- modules/oftc/manifests/puppet.pp
+++ modules/oftc/manifests/puppet.pp
@@ -1,5 +1,5 @@
-# run puppet every 30min from cron instead of daemonizing
 class oftc::puppet {
+  # run puppet every 30min from cron instead of daemonizing
   $hash = fqdn_rand(30)
   file { '/etc/cron.d/puppet':
     mode => 0644, owner => root, group => root,
@@ -10,5 +10,12 @@ class oftc::puppet {
     ensure => stopped,
     enable => false,
     require => File['/etc/cron.d/puppet'],
+  }
+
+  augeas { 'puppet.conf':
+    context => '/files/etc/puppet/puppet.conf',
+    changes => [
+      'set main/pluginsync true', # default in puppet 3.x
+    ],
   }
 }
