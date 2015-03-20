437e32c8434994d6c001315d8ec78943d2600f0c
Christoph Berg <myon@debian.org>
2015-03-20 13:31:41 +0100

    munin tls

--- modules/oftc/manifests/munin.pp
+++ modules/oftc/manifests/munin.pp
@@ -22,6 +22,42 @@ class oftc::munin {
     notify => Service['munin-node'],
   }
 
+  file_line { 'tls enabled':
+    path => '/etc/munin/munin-node.conf',
+    line => 'tls enabled',
+    match => '^tls ',
+    require => Package['munin-node'],
+    notify => Service['munin-node'],
+  }
+  file_line { 'tls_verify_certificate no':
+    path => '/etc/munin/munin-node.conf',
+    line => 'tls_verify_certificate no',
+    match => '^tls_verify_certificate ',
+    require => Package['munin-node'],
+    notify => Service['munin-node'],
+  }
+  file_line { 'tls_ca_certificate /etc/ssl/certs/thishost.pem':
+    path => '/etc/munin/munin-node.conf',
+    line => 'tls_ca_certificate /etc/ssl/certs/thishost.pem',
+    match => '^tls_ca_certificate ',
+    require => Package['munin-node'],
+    notify => Service['munin-node'],
+  }
+  file_line { 'tls_private_key /etc/ssl/private/thishost.key':
+    path => '/etc/munin/munin-node.conf',
+    line => 'tls_private_key /etc/ssl/private/thishost.key',
+    match => '^tls_private_key ',
+    require => Package['munin-node'],
+    notify => Service['munin-node'],
+  }
+  file_line { 'tls_certificate /etc/ssl/certs/thishost.pem':
+    path => '/etc/munin/munin-node.conf',
+    line => 'tls_certificate /etc/ssl/certs/thishost.pem',
+    match => '^tls_certificate ',
+    require => Package['munin-node'],
+    notify => Service['munin-node'],
+  }
+
   munin::plugin {[
     'df',
     'df_abs',
