40208fcd66846392df00841096c92d90314aaf9b
Christoph Berg <myon@debian.org>
2015-03-26 00:04:31 +0100

    Add pam limits file

--- modules/oftc/manifests/irc.pp
+++ modules/oftc/manifests/irc.pp
@@ -6,4 +6,9 @@ class oftc::irc {
     'zlib1g-dev',
     'libssl-dev',
   ]
+
+  file { '/etc/security/limits.d/oftc_limits.conf':
+    mode => 0644, owner => root, group => root,
+    source => "puppet:///modules/oftc/oftc_limits.conf",
+  }
 }
