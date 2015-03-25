4fb394cd440183edbd4a7a1f7d9427cb4d6095df
Christoph Berg <myon@debian.org>
2015-03-26 00:04:50 +0100

    Add db.debian.org repository

--- modules/oftc/manifests/udldap.pp
+++ modules/oftc/manifests/udldap.pp
@@ -1,4 +1,21 @@
 class oftc::udldap {
+  apt::key { 'db.debian.org':
+    key => 'D984518A0DCB4EEC519573DF661EBB0E456D79AB',
+    key_content => template('oftc/db.debian.org.asc'),
+  }
+
+  apt::source { 'db.debian.org':
+    location => 'http://db.debian.org/debian-admin',
+    release => 'debian-all',
+    repos => 'main',
+    include_src => false,
+    require => Apt::Key['db.debian.org'],
+  }
+
+  package { 'userdir-ldap':
+    require => Apt::Source['db.debian.org'],
+  }
+
   ensure_packages [
     'libnss-db',
   ]
