c274def15e6abeda4b92a6733709f1f2dfe24580
Christoph Berg <myon@debian.org>
2015-04-03 23:48:40 +0200

    Munin plugins need the package so the directory is there

--- modules/munin/manifests/plugin.pp
+++ modules/munin/manifests/plugin.pp
@@ -2,6 +2,7 @@ define munin::plugin ($plugin = $title) {
   file { "/etc/munin/plugins/$plugin":
     ensure => link,
     target => "/usr/share/munin/plugins/$plugin",
+    require => Package['munin-node'],
     notify => Service['munin-node'],
   }
 }
