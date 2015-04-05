1c315850863235e42ddc48527376ed5199a22ed9
Christoph Berg <myon@debian.org>
2015-04-05 22:57:47 +0200

    Deploy recvconf push key

--- modules/oftc/manifests/recvconf.pp
+++ modules/oftc/manifests/recvconf.pp
@@ -14,4 +14,17 @@ class oftc::recvconf {
     mode => 0755, owner => root, group => root,
     source => "puppet:///modules/oftc/recvconf",
   }
+
+  file { '/root/.ssh':
+    mode => 0700, owner => root, group => root,
+    ensure => directory,
+  }
+
+  $configserverips = join(hiera("configserverips"), ',')
+  file_line { 'recvconf push key':
+    path => '/root/.ssh/authorized_keys',
+    line => template("oftc/pushconfig.key"),
+    match => 'pushconfig@oftc',
+    require => File['/root/.ssh'],
+  }
 }
