5a5d1754ee3e60d913df2d03dbeedd300f6d4edb
Christoph Berg <myon@debian.org>
2015-04-14 23:04:58 +0200

    Skip postfix

--- manifests/site.pp
+++ manifests/site.pp
@@ -14,9 +14,9 @@ node default {
     include oftc::oftcdns
   }
 
-  if !($::hostname in hiera('specialmta')) {
-    include postfix
-  }
+#  if !($::hostname in hiera('specialmta')) {
+#    include postfix
+#  }
 
   if $::hostname in hiera('vservers') {
     include oftc::munin_v4fix
