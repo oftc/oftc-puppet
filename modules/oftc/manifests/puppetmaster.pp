01a430f27010bf135425fb8379af06d638687912
Christoph Berg <myon@debian.org>
2015-05-03 14:20:16 +0200

    Install ansible on puppetmaster

--- modules/oftc/manifests/puppetmaster.pp
+++ modules/oftc/manifests/puppetmaster.pp
@@ -1,4 +1,6 @@
 class oftc::puppetmaster {
+  include ansible
+
   file { '/etc/cron.daily/puppetmaster':
     mode => 0755, owner => root, group => root,
     source => "puppet:///modules/oftc/puppetmaster.cron.daily",
