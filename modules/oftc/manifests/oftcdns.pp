32437fac77f50b3c2c4084c176e182ea45c2e9c1
Christoph Berg <myon@debian.org>
2015-05-10 19:44:02 +0200

    redirect oftcdns mail

--- modules/oftc/manifests/oftcdns.pp
+++ modules/oftc/manifests/oftcdns.pp
@@ -2,4 +2,11 @@ class oftc::oftcdns {
   ensure_packages ([
     'kwalify',
   ])
+
+  file_line { 'oftcdns mail alias':
+    path => '/etc/aliases',
+    line => "oftcdns: root\n",
+    match => "^oftcdns:",
+    notify => Exec['newaliases'],
+  }
 }
