d5821ca4786e0c965e5db90c05a4e73136df1d37
Christoph Berg <myon@debian.org>
2016-04-09 20:52:46 +0200

    Install DH params and default symlinks to snakeoil certs

--- modules/oftc/manifests/cert.pp
+++ modules/oftc/manifests/cert.pp
@@ -18,4 +18,30 @@ class oftc::cert {
     target => "$::fqdn.key",
     require => Package['ca-certificates'],
   }
+
+  # create symlinks to snakeoil certs, can be replaced with better certs later
+  exec { "/etc/ssl/private/$::fqdn.key":
+    command => "/bin/ln -s ssl-cert-snakeoil.key /etc/ssl/private/$::fqdn.key",
+    creates => "/etc/ssl/private/$::fqdn.key",
+    require => Package['ca-certificates'],
+  }
+
+  exec { "/etc/ssl/certs/$::fqdn-chained.pem":
+    command => "/bin/ln -s ssl-cert-snakeoil.pem /etc/ssl/certs/$::fqdn-chained.pem",
+    creates => "/etc/ssl/certs/$::fqdn-chained.pem",
+    require => Package['ca-certificates'],
+  }
+
+  # create DH parameter files
+  exec { "/etc/ssl/certs/dh512":
+    command => "/usr/bin/openssl dhparam -out /etc/ssl/certs/dh512 512",
+    creates => "/etc/ssl/certs/dh512",
+    require => Package['ca-certificates'],
+  }
+
+  exec { "/etc/ssl/certs/dh1024":
+    command => "/usr/bin/openssl dhparam -out /etc/ssl/certs/dh1024 1024",
+    creates => "/etc/ssl/certs/dh1024",
+    require => Package['ca-certificates'],
+  }
 }
