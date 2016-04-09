698e0538527c0abce976e775167b684f031aaa4c
Christoph Berg <myon@debian.org>
2016-04-09 15:54:18 +0200

    Generate host list for letsencrypt.sh

--- modules/oftc/manifests/configmaster.pp
+++ modules/oftc/manifests/configmaster.pp
@@ -4,4 +4,11 @@ class oftc::configmaster {
   include smokeping
   include userdir-ldap::master
   include ::geodns
+
+  $hosts = hiera('hosts')
+
+  file { '/etc/oftc/acme/.domains.oftc.txt':
+    mode => 0644, owner => root, group => root,
+    content => template('oftc/hosts'),
+  }
 }
