447c3402f7781895d617ee65e80fe62d62b09704
Christoph Berg <myon@debian.org>
2016-07-09 10:47:42 +0200

    Run puppet only once per hour
    We don't actually need it that often, and this will also save disk space
    for reports on the server

--- modules/oftc/manifests/puppet.pp
+++ modules/oftc/manifests/puppet.pp
@@ -1,6 +1,6 @@
 class oftc::puppet {
-  # run puppet every 30min from cron instead of daemonizing
-  $hash = fqdn_rand(30)
+  # run puppet every 60min from cron instead of daemonizing
+  $hash = fqdn_rand(60)
   file { '/etc/cron.d/puppet':
     mode => 0644, owner => root, group => root,
     content => template('oftc/cron.d.puppet'),
