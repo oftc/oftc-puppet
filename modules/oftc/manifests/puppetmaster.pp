6a602529d604dca5782e0490c99bc9f560d8aa40
Christoph Berg <myon@debian.org>
2016-11-25 14:44:47 +0100

    puppetmaster: work around 8140 blocked

--- modules/oftc/manifests/puppetmaster.pp
+++ modules/oftc/manifests/puppetmaster.pp
@@ -7,8 +7,19 @@ class oftc::puppetmaster {
   }
 
   ferm::port { 'puppetmaster':
-    port => '8140',
+    port => '8140 7140',
   }
 
   include ferm::droneblmirror
+
+  # 7140: work around 8140 blocked
+  ensure_packages(['openbsd-inetd', 'netcat-openbsd'])
+
+  file { '/etc/inetd.conf':
+    mode => '0644', owner => root, group => root,
+    content => "7140 stream tcp nowait nobody /bin/nc nc -q0 ::1 8140\n",
+    notify => Service['inetd'],
+  }
+
+  service { 'inetd': }
 }
