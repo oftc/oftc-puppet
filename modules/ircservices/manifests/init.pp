9642b1feca74b14c6e5b7f4d6cdd5cf4ba69c34e
Christoph Berg <myon@debian.org>
2021-03-11 23:50:08 +0100

    ircservices: Remove obsolete ruby-openssl hack

--- modules/ircservices/manifests/init.pp
+++ modules/ircservices/manifests/init.pp
@@ -46,14 +46,5 @@ class ircservices {
   #  target => '../ircservices.service',
   #}
 
-  # /usr/lib/ruby/2.1.0/openssl/buffering.rb must not contain "coding: binary". wtf.
-  # RUBY ERROR: ArgumentError: unknown encoding name: binary
-  file_line { 'ruby-encoding-binary':
-    ensure => absent,
-    path => '/usr/lib/ruby/2.3.0/openssl/buffering.rb',
-    line => '# coding: binary',
-    require => Package['ruby-dev'],
-  }
-
   include oftc::postgresql
 }
